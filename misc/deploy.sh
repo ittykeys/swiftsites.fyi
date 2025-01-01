929611b4e205305e420133660470b37f88071b51
ittykeys <ittykeys@ittykeys.com>
2025-01-01 22:40:06 +0100

    Fuck it, we do the thing manually

--- misc/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Script to move files from one directory to another and reload apache
-
-sudo mv /data/www/ittykeys/swiftsites.fyi/misc/swiftsites.fyi.conf /etc/apache2/sites-available/swiftsites.fyi.conf
-./reloadapache.sh
